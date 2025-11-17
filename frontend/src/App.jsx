a62b2843bed4e150d89184ec1bc36849d0d8d74d
Rishishinde08 <rishikeshs2003@gmail.com>
2025-11-17 17:05:01 +0530

    home page trial

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pageLayout/homePage/homePage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,8 +10,9 @@ function App() {
   return (
     <>
       <h1 class="text-3xl font-bold underline">
-    Hello world! aniket
-  </h1>
+        Hello world! 
+      </h1>
+      <HomePage />
     </>
   )
 }
